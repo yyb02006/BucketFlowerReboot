0907bef09f42ee073071f998511d410b1e2a58ac
alwjrqns <yyb02006@gmail.com>
2024-03-30 05:27:51 +0900

    Feat : 클라이언트단에서 message엔드포인트 API요청 예시 작성
    - dev환경 Strict모드에서 API를 두 번 호출하지 않도록 ref로 고정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export default function Page() {
-  return <div>Hello NextJS!!</div>
+  const [reply, setReply] = useState<string>('')
+  const abortStrictRef = useRef(true)
+  const testCall = async () => {
+    const test = await (
+      await fetch('/api/messages', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          messages: [{ role: 'user', content: "Hello GPT! what's your version of GPT?" }],
+        }),
+      })
+    ).json()
+    setReply(test.response.choices[0].message.content)
+  }
+  useEffect(() => {
+    if (abortStrictRef.current) {
+      testCall()
+      abortStrictRef.current = false
+    }
+  }, [])
+  return (
+    <section className="text-amber-700">
+      Hello NextJS!!
+      <div>{reply}</div>
+    </section>
+  )
 }
